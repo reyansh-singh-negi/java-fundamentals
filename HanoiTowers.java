a46c69e6ea5cdd608e4e821320fd9892af511e8d
Reyansh Singh Negi <reyanshnnegi2008@gmail.com>
2026-07-02 15:48:00 +0530

    Added Towers of Hanoi algo

---
+++ HanoiTowers.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class HanoiTowers
+{
+    void Hanoi(int n , String src , String helper , String dest)
+    {
+        if(n==1)
+        {
+            System.out.println("Transferred disc "+n+" from "+src+" to "+dest);
+            return;
+        }
+
+        Hanoi(n-1,src,dest,helper);
+        System.out.println("Transferred disc "+n+" from "+src+" to "+dest);
+        Hanoi(n-1,helper,src,dest);
+    }
+
+    public static void main(String args[])
+    {
+        HanoiTowers ob = new HanoiTowers();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of discs you want in the Tower of Hanoi");
+        int n = sc.nextInt();
+        ob.Hanoi(n,"Source","Helper","Destination");
+    }
+}
